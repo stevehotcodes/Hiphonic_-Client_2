12c009714781fcc9072041f3e2e70f9ffc31eb06
Stephen Ondieki <stevehotcodes@gmail.com>
2024-03-04 15:00:28 +0300

    (feat)  members can join a group

--- src/features/Groups/Groups.jsx
+++ src/features/Groups/Groups.jsx
@@ -3,20 +3,27 @@ import './Groups.scss';
 import { FaEllipsis } from 'react-icons/fa6';
 // import MaskGroup from '../../assets/Mask Group.png';
 import { useDispatch } from 'react-redux';
-import { getGroups, getGroupsError, getGroupsStatus, selectAllGroups } from './groupSlice';
+// import { getGroups, getGroupsError, getGroupsStatus, selectAllGroups } from './groupAPI.js';
 import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import AddGroup from './addGroup';
 import Modal from '../../components/Modal/Modal';
 import '../../pages/Timeline/Addpost.scss';
 import groupPlaceholder from '../../assets/unsplash_photo-6.png'
+import { useAddGroupMembertoAGroupMutation, useGetGroupMembersQuery } from '../../features/Groups/groupMemberApi.js'
+import { ClipLoader, PuffLoader } from 'react-spinners';
+import { useGetGroupsQuery } from './groupAPI.js';
+import { ErrorToast, LoadingToast, SuccessToast, ToasterContainer } from '../../components/Toaster/Toaster.jsx';
+// import PuffLoader from 'react-spinners';
 const Groups = () => {
-  const dispatch = useDispatch();
-  const groups = useSelector(selectAllGroups);
-  const status = useSelector(getGroupsStatus);
-  const error = useSelector(getGroupsError);
-
   const [isModalOpen, setModalOpen] = useState(false);
+  const [joinedGroup,setJoinGroup]=useState(false);
+  
+
+
+  const [addGroupMembertoAGroup] = useAddGroupMembertoAGroupMutation();
+  const {data:groups, isError, isLoading}=useGetGroupsQuery()
+  console.log(`data:${groups}, error:${isError},isLoading:${isLoading}`)
 
   const openModal = () => {
     setModalOpen(true);
@@ -25,19 +32,49 @@ const Groups = () => {
   const closeModal = () => {
     setModalOpen(false);
   };
+  // useEffect(() => {
+  //   console.log('Effect triggered');
+  //   console.log('Status:', status);
+  //   if (status === 'idle') {
+  //     console.log('Dispatching getGroups');
+  //     dispatch(getGroups());
+  //   }
+  // }, [status, dispatch]);
+  
 
 
+  const handleJoinGroup=async(group)=>{
+    const {group_id}=group;   
+    console.log(group)
+ 
+          // console.log(response)
 
-  console.log(groups)
-  useEffect(() => {
-    if (status === 'idle') {
-        dispatch(getGroups());
-    }
-    console.log(getGroups)
-  }, [status, dispatch]);
+          try {
+            const response=await addGroupMembertoAGroup(group_id).unwrap();
+            console.log(response)
+              LoadingToast
+              SuccessToast(
+                response.message
+      )
+       
+            
+          } catch (error) {
+
+            console.log(error.data.message)
+            ErrorToast(error.data.message)
+             
+          }
+   
+
+
+  }
 
   return (
-    <div className='groups-container'>
+    <>
+    <ToasterContainer/>
+     {isError && <div>Error:{error}</div>}
+    
+     <div className='groups-container'>
       <div className='group-header-wrapper'>
         <div className='group-header'>
           <span>Groups</span>
@@ -66,7 +103,8 @@ const Groups = () => {
         </div>
         <span>See All</span>
         <div className="groups-card-container">
-        {Array.isArray(groups) &&
+        {isLoading  && (<PuffLoader color="#000" loading={true} size={150} />)}
+        {groups &&
           groups.map((group) => (
             
             <div className='group-card' key={group.group_id}>
@@ -84,7 +122,7 @@ const Groups = () => {
                 <img src={groupPlaceholder} alt='' /> 
               </div>
               <div className='join-group-btn'>
-                <button>Join group</button>
+                <button onClick={()=>handleJoinGroup(group)}>Join group</button>
               </div>
             </div>
           ))}
@@ -94,6 +132,11 @@ const Groups = () => {
 
       <div className='recent-activity'></div>
     </div>
+    
+    
+    
+    </>
+   
   );
 };
 
